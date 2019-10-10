918167a4631ded18664d563c62a89b0a4aabe17d
xiaohangzhan <xiaohangzhan@outlook.com>
2019-10-10 16:22:40 +0800

    initial

---
+++ source.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export PATH=/mnt/lustre/zhanxiaohang/deps/conda_envs/ssl-bench/bin:$PATH
+export LD_LIBRARY_PATH=/mnt/lustre/zhanxiaohang/deps/conda_envs/ssl-bench/lib:$LD_LIBRARY_PATH
+export PYTHONPATH=/mnt/lustre/zhanxiaohang/deps/conda_envs/ssl-bench/lib/python3.6/site-packages:$PYTHONPATH
